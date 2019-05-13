f857a8bbd0fc1147db03c18b0fa4a3d73af41592
Lars Hupel <lars.hupel@mytum.de>
2019-05-13 10:31:30 +0200

    use a shorter test

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ source "$(dirname "$0")/common.sh"
 cecho $white "Testing ..."
 
 RESULT="$(./test/test.cake.out)"
-[[ "$RESULT" = "True" ]] || die "Test failed"
+[[ "$RESULT" = "hi" ]] || die "Test failed"
 
 cecho $green "Test succeeded."
